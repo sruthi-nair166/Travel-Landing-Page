7916f9692b1d1269e48f4fea5cbb686e98386253
Sruthi <sruthivnair78@gmail.com>
2025-12-01 15:59:19 +0530

    refactored category card div with button and added hover styles, cleaned up header section code and fixed scroll not restoring to default after nav link click issue

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.getElementById("menu-btn");
 const menuTray = document.getElementById("menu-tray");
 const closeBtn = document.getElementById("close-btn");
+const menuLinks = document.querySelectorAll("#menu-tray a");
+
+console.log(menuLinks);
 
 menuBtn.addEventListener("click", () => {
   menuTray.classList.remove("hidden");
@@ -10,4 +13,12 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   menuTray.classList.add("hidden");
   document.body.classList.remove("no-scroll");
+});
+
+menuLinks.forEach((link) => {
+  console.log(link);
+  link.addEventListener("click", () => {
+    menuTray.classList.add("hidden");
+    document.body.classList.remove("no-scroll");
+  });
 });
